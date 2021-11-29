229b525e2aa197bb82b4f98ece42b6c1c5a87cb7
‘Katie <ktschr11@gmail.com>
2021-11-29 12:34:09 -0800

    initialized state

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState } from 'react'
 
 import Preview from '../../components/Preview/Preview'
 import Editor from '../../components/Editor/Editor'
@@ -7,6 +7,11 @@ import './Home.css'
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('')
+  const [subtitle, setSubtitle] = useState('')
+  const [font, setFont] = useState('Times New Roman')
+  const [align, setAlign] = useState('Left')
+  const [text, setText] = useState('')
 
   return (
     <main>
